19d4f4fa81d078f4a181de08109b6bea800e6a9e
Guillaume Humm <ghumm@k1r1p2.42mulhouse.fr>
2023-10-18 10:42:33 +0200

    ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,24 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ghumm <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/17 15:46:22 by ghumm             #+#    #+#             */
-/*   Updated: 2023/10/17 16:07:18 by ghumm            ###   ########.fr       */
+/*   Created: 2023/10/18 09:42:33 by ghumm             #+#    #+#             */
+/*   Updated: 2023/10/18 10:42:06 by ghumm            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-void	bzero(void *s, size_t n)
+#include <stddef.h>
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	unsigned char	*p;
+	char *dest1;
+	char *src1;
+	size_t i;
 
-	p = s;
-	while (n > 0)
-	{
-		*p = 0;
-		p++;
-		n--;
-	}
-}
+	i = 0;
+	dest1 = (char* ) dest;
+	src1 = (char* ) src;
+
+	while (i != n)
+		{
+		dest1[i] = src1[i];
+		i++;
+		}
+	dest1[n] = '\0';
+	return (dest);
+}/*
+#include <stdio.h>
+int main ()
+{
+	char dest[10];
+	char *src = "coucou c'est moi !";
+	ft_memcpy(dest, src, 10);
+	printf("%s\n", dest);
+
+}*/
