f30d99af3f06dee4cc3991f3e907705085ef111a
Guillaume Humm <ghumm@k1r3p2.42mulhouse.fr>
2023-10-20 12:56:39 +0200

    ft_strlcpy2

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,45 +1,44 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ghumm <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/18 14:31:50 by ghumm             #+#    #+#             */
-/*   Updated: 2023/10/20 11:27:44 by ghumm            ###   ########.fr       */
+/*   Created: 2023/10/18 16:28:50 by ghumm             #+#    #+#             */
+/*   Updated: 2023/10/20 12:52:01 by ghumm            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stddef.h>
-#include <string.h>
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+
+size_t	ft_strlcat (char *dst , const char *src, size_t size)
 {
-	size_t	v;
+	size_t	v1;
+	size_t	v2;
 
-	v = 0;
-	if (size == 0)
-		return strlen(src);
-	while (size - 1 > v && src[v] != 0)
+	v1 = 0;
+	v2 = 0;
+	
+	while (dst[v1] != 0)
 	{
-		dst[v] = src[v];
-		v++;
+		v1++;
 	}
-	dst[v] = '\0';
-	v = 0;
-	while (src[v] != 0)
-		v++;
-	return v;	
-}
+	while ((size-- - v1 - 1) && src[v2] != 0)
+	{
+		dst[v1++] = src[v2++];
+	}
+	dst[v1] = '\0';
 
+	return (v1 + v2);
+
+}
 #include <stdio.h>
 int main()
 {
-	char dst[12];
-	char *src = "Coucou c'est moi !";
+	char *dst = "abcd";
+	char *src = "12345";
 
-	printf("%ld\n", ft_strlcpy(dst, src, 2));
+	printf("%ld\n", ft_strlcat(dst, src, 15));
 	printf("%s\n", dst);
-    printf("%ld\n", strlen(src));
-    printf("%ld\n", strlen(dst));
-
-
+	printf("%s\n", src);
 }
