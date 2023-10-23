23b29cffbc2323a80bbd0fda854f19361f507f23
Guillaume Humm <ghumm@k1r3p5.42mulhouse.fr>
2023-10-23 16:13:06 +0200

    ft_atoi

---
+++ ft_atoi.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ghumm <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/23 13:19:02 by ghumm             #+#    #+#             */
+/*   Updated: 2023/10/23 16:12:22 by ghumm            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(const char *nptr)
+{
+	int sign = 1;
+	int valeur = 0;
+
+	while (*nptr >= 9 && *nptr <= 13 || *nptr == 32)
+		nptr++;
+
+	if (*nptr == 45 || *nptr == 43)
+	{
+		if (*nptr == 45)
+			sign *= -1;
+		nptr++;
+	}
+
+	while (*nptr >= 48 && *nptr <= 57)
+	{
+		valeur = (valeur * 10) + (*nptr - 48);
+		nptr++;
+	}
+	return (valeur * sign);
+}
+
+#include <stdlib.h>
+#include <stdio.h>
+int main()
+{
+	const char *a = "\t\n 	-123456";
+
+	int val = ft_atoi(a);
+	int val2 = atoi(a);
+
+	printf("%d\n", val);
+	printf("%d\n", val2);
+}
