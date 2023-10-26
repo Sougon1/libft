06a1ec8327002053830fc37f4a0b8a900d50df7b
Sougon <guillaume.humm@yahoo.fr>
2023-10-26 15:15:21 +0200

    ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -13,5 +13,30 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
+	size_t	start;
+	size_t	end;
+	size_t	dif;
+	char	*ptr;i
+	size_t	i;
 
+	start = 0;
+	end = ft_strlen(s1);
+	if (s1 == NULL || set == NULL)
+		return (NULL);
+	while (s1[start] && ft_strchr(set, s1[start])
+		start++;
+	while (end > start && ft_strchr(set, s1[end -1]))
+		end--;
+	dif = end - start;
+	ptr = (char *)malloc(dif + 1);
+	if (!(ptr))
+		return (NULL);
+	i = 0;
+	while (i < dif)
+	{	
+		ptr[i] = s1[start + i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
 }
