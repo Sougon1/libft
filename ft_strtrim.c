9c229d64dc43f8c2a93b2a83506a5df0b5886941
Guillaume Humm <ghumm@k1r3p4.42mulhouse.fr>
2023-10-27 15:07:33 +0200

    maj trim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: ghumm <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 16:44:51 by ghumm             #+#    #+#             */
-/*   Updated: 2023/10/27 15:00:54 by ghumm            ###   ########.fr       */
+/*   Updated: 2023/10/27 15:07:08 by ghumm            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -17,7 +17,6 @@ char	*ft_strtrim(char const *s1, char const *set)
 	size_t	end;
 	size_t	dif;
 	char	*ptr;
-	size_t	i;
 
 	start = 0;
 	end = ft_strlen(s1);
@@ -31,12 +30,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 	ptr = (char *)malloc(dif + 1);
 	if (!(ptr))
 		return (NULL);
-	i = 0;
-	while (i < dif)
-	{	
-		ptr[i] = s1[start + i];
-		i++;
-	}
-	ptr[i] = '\0';
+	ft_memcpy(ptr, s1 + start, dif);
+	ptr[dif] = '\0';
 	return (ptr);
 }
